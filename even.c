e56437a151b18501dce97b6405f9a67b9839f3d1
Saipreetha <saipreethaekb@gmail.com>
2016-12-22 11:57:27 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    int n;
+    printf("enter the no");
+    scanf("%d",&n);
+    if(n%2==0)
+{
+printf("even no");
+}
+
+else
+  printf("odd no");
+
+}
